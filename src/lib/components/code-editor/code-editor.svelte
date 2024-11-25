43b0dbe6ccc6990b769e16b59cbe7842d1e3d62b
indra saputra idrus <indrasaputraidrus@gmail.com>
2024-11-25 16:03:50 +0000

    chore: refactor editor

--- src/lib/components/code-editor/code-editor.svelte
+++ src/lib/components/code-editor/code-editor.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import clsx from 'clsx';
+	import { fade } from 'svelte/transition';
 	import { createHighlighterCore, type HighlighterCore } from 'shiki/bundle/web';
 	import { createOnigurumaEngine } from 'shiki/engine/oniguruma';
 	import js from 'shiki/langs/javascript.mjs';
 	import githubDark from 'shiki/themes/github-dark.mjs';
+
+	let hl: HighlighterCore | undefined = $state();
 
 	let editor = $state({
 		title: 'Untitled-1',
 		code: `console.log('Hello world')`,
 		lang: 'javascript'
 	});
-
-	let hl: HighlighterCore | undefined = $state();
 
 	let codeRows = $derived((editor.code.match(/\n/g)?.length ?? 1) + 1);
 	let highlighterCode = $derived(
@@ -28,21 +29,24 @@
 		})
 	);
 
-	$effect(() => {
-		createHighlighterCore({
+	async function loadHighlighter() {
+		const highlighter = await createHighlighterCore({
 			themes: [githubDark],
 			langs: [js],
 			engine: createOnigurumaEngine(import('shiki/wasm'))
-		}).then((highlighter) => {
-			hl = highlighter;
 		});
 
+		hl = highlighter;
+	}
+
+	$effect(() => {
+		loadHighlighter();
 		return () => hl?.dispose();
 	});
 </script>
 
 {#snippet heading()}
-	<div class="grid grid-cols-3 gap-4 border-b border-slate-500/50 px-5 py-4">
+	<div class="grid grid-cols-[1fr_2fr_1fr] gap-4 border-b border-slate-500/50 px-5 py-4">
 		<div class="flex items-center gap-2">
 			{#each { length: 3 } as _}
 				<span class="size-3.5 rounded-full bg-slate-500/50"></span>
@@ -50,6 +54,9 @@
 		</div>
 		<input
 			type="text"
+			spellcheck="false"
+			autocomplete="off"
+			autocapitalize="off"
 			onclick={(e) => {
 				e.currentTarget.select();
 			}}
@@ -65,11 +72,7 @@
 {#snippet codeBlock()}
 	<div class="codeblock">
 		<div class="p-4 font-mono">
-			{#if highlighterCode}
-				{@html highlighterCode}
-			{:else}
-				<p class="text-sm opacity-60">Loading...</p>
-			{/if}
+			{@html highlighterCode}
 		</div>
 		<textarea
 			style="caret-color: white;"
@@ -84,7 +87,9 @@
 	</div>
 {/snippet}
 
+{#if highlighterCode}
 	<div
+		in:fade={{ duration: 200 }}
 		class={clsx(
 			'grid grid-cols-1',
 			'mx-auto w-full max-w-2xl',
@@ -96,6 +101,7 @@
 		{@render heading()}
 		{@render codeBlock()}
 	</div>
+{/if}
 
 <style>
 	.codeblock {
